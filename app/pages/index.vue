f8c29a61a8bd7edaf273c00eaea1626de01f98c3
mizuki_r <ry.mizuki@gmail.com>
2018-03-24 10:36:17 +0900

    chore(pug): allow use pug on templates

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,19 +1,11 @@
-<template>
-  <el-container style="hight: 500px; border: 1px solid #eee">
-    <el-header>
-      <span>animeshi</span>
-    </el-header>
-    <el-main>
-
-    </el-main>
-    <el-footer>
-      <p style="text-align: center">
-        <small>&copy; 2018 animeshi.ry-m.com</small>
-      </p>
-
-    </el-footer>
-  </el-container>
-
+<template lang="pug">
+  el-container(style="hight: 500px; border: 1px solid #eee")
+    el-header
+      span animeshi
+    el-main
+    el-footer
+      p(style="text-align: center")
+        small &copy; 2018 animeshi.ry-m.com
 </template>
 
 <script>
